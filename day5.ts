47a5108593bab028d68542348b4e1b63cd152f3b
Bram <b.j.h.otte@student.utwente.nl>
2022-12-05 11:02:10 +0100

    init

---
+++ day5.ts
@@ -0,0 +1,54 @@
+export {}
+const input: string = await Deno.readTextFile("input5.txt");
+`
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`
+
+
+const [top, bot] = input.split("\n\n").map(line => line.replace("\r", ""));
+
+let stacks: string[][] = [];
+{
+    const lines = top.split("\n").map(line => line.replace("\r", ""));
+    const last = lines[lines.length-1];
+    const indexes = last.split(/\s+/)
+        .map(v => {
+            return last.indexOf(v);
+        }).slice(1, -1);
+    for (const line of lines.slice(0,-1)) {
+        console.log(line);
+        indexes.forEach((v, i) => {
+            if (line[v] === " ") {
+                return
+            }
+            (stacks[i] ??= []).push(line[v]);
+        })
+    }
+}
+for (const stack of stacks) {
+    stack.reverse();
+}
+
+
+const moves: [number, number, number][] = [];
+for (const line of bot.split("\n")) {
+    const [move, count, from, i, to, j] = line.split(/\s+/);
+    moves.push([Number(count), Number(i) - 1, Number(j) - 1]);
+}
+
+for (const [count, from, to] of moves) {
+    for (let i = 0; i < count; i++) {
+        stacks[to].push(stacks[from].pop() as any);
+    }
+}
+
+
+console.log(stacks.map(v => v[v.length-1]).join(""));
+
